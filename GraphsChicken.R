da8a8f80248cb462a56ceddc9c3dbc2b68e49aad
FloorzUU <113982041+FloorzUU@users.noreply.github.com>
2022-09-20 13:37:42 +0200

    Delete GraphsChicken.R

--- GraphsChicken.R
+++
@@ -1,6 +0,0 @@
-f <- read.csv("C:\\Users\\Floor van der Zalm\\Documents\\MASTER\\BiBc essentials\\week2\\Scorings_kip.csv", sep = ';')
-
-library('ggplot2')
-
-ggplot(data = f) + 
-  geom_point( mapping = aes(x = FeatherColor, y = Weight))
